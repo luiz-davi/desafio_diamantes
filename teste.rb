cb0f463ee892df2257c26855f950507c1c35fbfa
Luiz Davi <luizd4398@gmail.com>
2022-01-24 14:08:58 -0300

    Desafio concluido

---
+++ teste.rb
@@ -0,0 +1,20 @@
+expressao = "<<.<<..>><>><.>.>.<<.>.<.>>>><>><>>"
+areai = expressao.count(".")
+expressao = expressao.delete(".")
+expressao = expressao.chars
+
+diamantes = 0
+index = 0
+while index < expressao.length
+    if expressao[index] == "<" and expressao[index + 1] == ">"
+        diamantes += 1
+        expressao.delete_at(index)
+        expressao.delete_at(index)
+        index = 0
+        next
+    end
+    index += 1
+end
+Z
+puts "Há #{diamantes} diamante(s) na expressão"
+puts "Há #{areai} grão(s) de areai na espressão"
